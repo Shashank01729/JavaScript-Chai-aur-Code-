5cceec89c0ea5d37f3a67a2734d79e43c41a11b6
Shashank01729 <117083031+Shashank01729@users.noreply.github.com>
2024-01-18 20:26:44 +0530

    Add files via upload

---
+++ Lecture-28 (While and Do While Loop)/While_and_do_while_loop.js
@@ -0,0 +1,23 @@
+// while Loop
+let index=0;
+while(index<=10){
+    console.log(`Value of index is ${index}`);
+    index+=2;
+}
+
+// whle loop in arrays
+let MC=["Ayanokouji", "Hachiman", "Yuji"]
+let arr=0;
+while(arr<MC.length){
+    console.log(`Value is ${MC[arr]}`);
+    arr++;
+}
+
+
+// do while Loop
+// Will run atleast one time
+let score=11;
+do{
+    console.log(`Score is ${score}`);
+    score++;
+}while(score<=10);
